cdda2c544134b62483a7f4f89e17232d6b40d0da
“Pooja <pakcutie14@gmail.com>
2022-11-07 21:10:27 -0700

    Updated brodcast activity

--- Project/CSE545Project1/app/src/main/java/com/example/cse545project1/BroadcastActivity.kt
+++ Project/CSE545Project1/app/src/main/java/com/example/cse545project1/BroadcastActivity.kt
@@ -7,12 +7,17 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import com.example.cse545project1.helpers.BrdReceiver
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class BroadcastActivity : AppCompatActivity() {
     private val _intentAction = "com.example.CSE545"
+    private val _intentMsg = "Hello from broadcast!"
 
     private val _receiver = BrdReceiver()
     private val _intentFilter = IntentFilter(_intentAction)
+
+    private val _dateFormatter = SimpleDateFormat("HH:mm")
 
     // Overrides
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,15 +37,28 @@ class BroadcastActivity : AppCompatActivity() {
 
     // Callback function for the receiver
     private fun brdCallback(i: Intent) {
+        // Extract elements from the intent
+        val intentAction = i.action
+        val intentMsg = i.getStringExtra("Message")
+        val intentTime = i.getStringExtra("Time")
+
         // Update textview
         val tv = findViewById<TextView>(R.id.broadcast_msg)
         // Set textview to the action of the intent
-        tv.text = i.action
+        tv.text = "Action: $intentAction \n" +
+                "Message: $intentMsg \n" +
+                "Time: $intentTime"
     }
 
     fun sendBroadcast(v: View) {
         val i = Intent()
         i.action = _intentAction
+        i.putExtra("Message", _intentMsg)
+        i.putExtra("Time", _dateFormatter.format(Date()))
         sendBroadcast(i)
+
+        // Update on UI
+        val tv = findViewById<TextView>(R.id.brd_sent_msg)
+        tv.text = "Broadcasted message: $_intentMsg"
     }
 }
