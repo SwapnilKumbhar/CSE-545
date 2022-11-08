12fa69defe4722563c4fa1c8a65e1599525ee359
“Pooja <pakcutie14@gmail.com>
2022-11-07 20:13:27 -0700

    fix(Main,Service): Added time on main and fixed logic on Service

--- Project/CSE545Project1/app/src/main/java/com/example/cse545project1/MainActivity.kt
+++ Project/CSE545Project1/app/src/main/java/com/example/cse545project1/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.cse545project1
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 import android.view.View
+import android.widget.TextView
+import com.example.cse545project1.services.TimeService
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+    private val _date = Date()
+    private val _dateFormatter = SimpleDateFormat("HH:mm")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +27,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ContentProviderActivity::class.java)
             startActivity(intent)
         }
+
+        // Update text view with current time
+        val currTimeTv = findViewById<TextView>(R.id.main_curr_time)
+        currTimeTv.text = _dateFormatter.format(_date)
     }
 
     // Callbacks
