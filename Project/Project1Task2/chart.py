552c4c84b7642424e69f80dabfb44703c3c6d4d0
“Pooja <pakcutie14@gmail.com>
2022-11-15 22:31:14 -0700

    Improved line chart

--- Project/Project1Task2/chart.py
+++ Project/Project1Task2/chart.py
@@ -6,9 +6,14 @@ def plot_chart(number_of_perms, number_of_apps):
     number_of_perms_arr = np.array(number_of_perms)
     number_of_apps_arr = np.array(number_of_apps)
     
+    plt.rcParams["figure.figsize"] = (15,10)
+
     plt.title("Permission statistics")
     plt.xlabel("Number of permissions requested by apps")
     plt.ylabel("Number of apps requesting respective permissions")
+
+    plt.xticks(np.arange(min(number_of_perms), max(number_of_perms)+1, 3.0))
+    plt.yticks(np.arange(min(number_of_apps), max(number_of_apps)+1, 1.0))
 
     plt.plot(number_of_perms_arr, number_of_apps_arr)
     plt.savefig("line_chart.png")
