6bfadc88fe4a3b9cfa018dd52f631810e257171f
“Pooja <pakcutie14@gmail.com>
2022-11-17 18:33:16 -0700

    Fixed line chart logic

--- Project/Project1Task2/chart.py
+++ Project/Project1Task2/chart.py
@@ -23,18 +23,15 @@ def create_line_chart(app_perms):
 
     unique_perm_occurences = list({len(perms) for perms in app_perms})
 
-    perm_occurences_to_index_dict = {}
-
-    for index, unique_occurence in enumerate(unique_perm_occurences):
-        perm_occurences_to_index_dict[unique_occurence] = index
-
-    number_of_apps = [0] * len(unique_perm_occurences)
+    max_perms = max(unique_perm_occurences)
+    
+    perms = [i for i in range(0, max_perms + 1)]
+    number_of_apps = [0] * len(perms)
 
     for app_perm in app_perms:
         number_of_perms = len(app_perm)
-        occurences_index = perm_occurences_to_index_dict[number_of_perms]
-        number_of_apps[occurences_index] += 1
+        number_of_apps[number_of_perms] += 1
     
-    plot_chart(unique_perm_occurences, number_of_apps)
+    plot_chart(perms, number_of_apps)
     
     
