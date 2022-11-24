9aa34d12295ac93faef9c4b1086815d1f34b6a52
Swapnil Kumbhar <swapsk05@gmail.com>
2022-11-24 01:14:57 -0700

    feat(features): Added camera usage, fixed locations

--- Project/Project1Task3/feature_extraction/features/has_coarse_location.py
+++ Project/Project1Task3/feature_extraction/features/has_coarse_location.py
@@ -6,12 +6,11 @@ def get_features(manifest: _Element, feat_vector):
         feat_vector.append(-1)
         return
     permissions = manifest.findall("uses-permission")
+    perm_name = "{http://schemas.android.com/apk/res/android}name"
     for perm in permissions:
-        if (
-            perm.attrib["{http://schemas.android.com/apk/res/android}name"]
-            == "android.permission.ACCESS_COARSE_LOCATION"
-        ):
-            feat_vector.append(1)
-            return
+        if perm_name in perm.attrib:
+            if perm.attrib[perm_name] == "android.permission.ACCESS_COARSE_LOCATION":
+                feat_vector.append(1)
+                return
 
     feat_vector.append(0)
