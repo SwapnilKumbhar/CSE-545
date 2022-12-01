392d82d340a87192f6be7d6738a15756a007b7b8
“Pooja <pakcutie14@gmail.com>
2022-12-01 03:03:58 -0700

    Added comments to graph dot and image code

--- Project/Project2/graph/visualize/dot.py
+++ Project/Project2/graph/visualize/dot.py
@@ -5,6 +5,7 @@ from graphviz import Digraph
 def create_dot(nodes: list[Node]) -> str:
     dot = Digraph(format="dot")
 
+    # Add nodes and store node is to name mapping in a dict for creating edges later
     node_names_dict = {}
 
     for index, node in enumerate(nodes):
@@ -14,14 +15,14 @@ def create_dot(nodes: list[Node]) -> str:
 
         node_names_dict[node] = node_name
 
+    # Iterate through all edges of every node (only in_edges is enough)
     for node in nodes:
         for edge in node.in_edges:
             from_node_name = node_names_dict[edge.from_node]
             end_node_name = node_names_dict[edge.end_node]
             dot.edge(from_node_name, end_node_name)
 
-    print(dot.source)
-
+    # Save dot file and return the file name
     file_name = "output"
     dot.render(filename=file_name)
 
